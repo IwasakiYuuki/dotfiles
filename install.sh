6041188d72bad35e4f27dff65973dd61b9d5e17c
IwasakiYuuki <2445yuuki@gmail.com>
2023-09-02 12:30:09 +0900

    wip

--- install.sh
+++ install.sh
@@ -1,37 +1,115 @@
 #!/bin/bash
 
-SCRIPT_DIR=$(cd "$(dirname "$0")" || exit; pwd)
+# ----------------------------------
+# Environments
+# ----------------------------------
+
+# XDG
+export XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-$HOME/.config}
+export XDG_DATA_HOME=${XDG_DATA_HOME:-$HOME/.local/share}
+export XDG_CACHE_HOME=${XDG_CACHE_HOME:-$HOME/.cache}
+export XDG_STATE_HOME=${XDG_STATE_HOME:-$HOME/.local/state}
+
+mkdir -p ${XDG_CONFIG_HOME}
+mkdir -p ${XDG_DATA_HOME}
+mkdir -p ${XDG_CACHE_HOME}
+mkdir -p ${XDG_STATE_HOME}
 
 # ----------------------------------
-# GENERAL
+# Requirements
 # ----------------------------------
-# Install fzf
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
+install_requirements() {
+  case "$1" in
+    "Ubuntu"*)
+      sudo apt update
+      if ! command -v curl &> /dev/null && ! command -v wget &> /dev/null ; then
+        echo "[INFO] install curl"
+        sudo apt install -y curl
+      fi
+      if ! command -v git &> /dev/null; then
+        echo "[INFO] install git"
+        sudo apt install -y git
+      fi
+      ;;
+    *)
+      echo "Unsupported OS: ${OS}"
+      ;;
+  esac
+}
 
 # ----------------------------------
-# TMUX
+# Zsh
 # ----------------------------------
-# Install Tmux Plugin Manager
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+install_zsh() {
+  case "$1" in
+    "Ubuntu"*)
+      # zsh
+      ## install
+      if ! command -v zsh &> /dev/null; then
+        echo "[INFO] install and change default shell to zsh"
+        sudo apt install -y zsh
+        chsh -s $(which zsh)
+      fi
+      ## setting (put dotfiles and create symbolic link)
+      if [ ! -d ${XDG_CONFIG_HOME}/zsh ]; then
+        ln -s ${XDG_CONFIG_HOME}/dotfiles/zsh ${XDG_CONFIG_HOME}/zsh
+      fi
+      if [ -f ${HOME}/.zshenv ]; then
+        mv ${HOME}/.zshenv ${HOME}/.zshenv.old
+      fi
+      ln -s ${XDG_CONFIG_HOME}/zsh/.zshenv ${HOME}/.zshenv
+      source ${HOME}/.zshenv
+      ## zinit
+      bash -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
+      ;;
+    *)
+      echo "Unsupported OS: ${OS}"
+      ;;
+  esac
+}
 
 # ----------------------------------
-# ZSH
+# Nvim
 # ----------------------------------
-# Install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-# Install zsh power line
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+install_nvim() {
+  case "$1" in
+    "Ubuntu"*)
+      # nvim
+      ## manual install (latest)
+      if ! command -v nvim &> /dev/null; then
+        curl -o ${HOME}/nvim.appimage -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+        sudo chmod u+x ${HOME}/nvim.appimage
+        (cd ${HOME} && ./nvim.appimage --appimage-extract)
+        rm ${HOME}/nvim.appimage
+        sudo mv ${HOME}/squashfs-root ${XDG_DATA_HOME}/squashfs-root
+        mkdir -p ${HOME}/.local/bin
+        if [ ! -f ${HOME}/.local/bin/nvim ]; then
+          ln -s ${XDG_DATA_HOME}/squashfs-root/AppRun ${HOME}/.local/bin/nvim
+        fi
+      fi
+      ## setting
+      if [ ! -d ${XDG_CONFIG_HOME}/nvim ]; then
+        ln -s ${XDG_CONFIG_HOME}/dotfiles/nvim ${XDG_CONFIG_HOME}/nvim
+      fi
+      ;;
+    *)
+      echo "Unsupported OS: ${OS}"
+      ;;
+  esac
+}
 
-# ----------------------------------
-# Install dotfiles / setting files
-# ----------------------------------
-# WSL
-if [[ "$(uname -r)" == *microsoft* ]]; then
-  ln -s "$SCRIPT_DIR/wsl/wsl.conf" "$HOME/wsl.conf"
+# Install by OS
+if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+  if [ -f /etc/os-release ]; then
+      . /etc/os-release
+      OS=$NAME
+  fi
+  install_requirements ${OS}
+  ## clone dotfiles repository
+  if [ ! -d ${XDG_CONFIG_HOME}/dotfiles ]; then
+    git clone https://github.com/IwasakiYuuki/dotfiles.git ${XDG_CONFIG_HOME}/dotfiles
+  fi
+  install_zsh ${OS}
+  install_nvim ${OS}
 fi
 
-# All
-ln -s "$SCRIPT_DIR/nvim" "$HOME/.config/nvim"
-ln -s "$SCRIPT_DIR/.zshrc" "$HOME/.zshrc"
-ln -s "$SCRIPT_DIR/.tmux.conf" "$HOME/.tmux.conf"
---
+++ zsh/.zshenv
@@ -0,0 +1,6 @@
+export XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-$HOME/.config}
+export XDG_DATE_HOME=${XDG_DATE_HOME:-$HOME/.local/share}
+export XDG_CACHE_HOME=${XDG_CACHE_HOME:-$HOME/.cache}
+export XDG_STATE_HOME=${XDG_STATE_HOME:-$HOME/.local/state}
+
+export ZDOTDIR=${XDG_CONFIG_HOME}/zsh
